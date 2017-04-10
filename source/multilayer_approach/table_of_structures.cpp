51a97912ece076de2f8a7769be62d87030cb7ba3
svech <svechnikovmv@gmail.com>
2017-04-10 23:47:26 +0300

    adding materials

--- source/multilayer_approach/table_of_structures.cpp
+++ source/multilayer_approach/table_of_structures.cpp
@@ -15,50 +15,118 @@ Table_Of_Structures::Table_Of_Structures(QMap<QString, Table_Of_Structures*>* ru
 void Table_Of_Structures::create_Main_Layout()
 {
 	main_Layout = new QHBoxLayout(this);
-		main_Layout->setContentsMargins(4,3,2,3);	
-	create_Table();
+		main_Layout->setContentsMargins(0,0,0,0);
+	create_Table();	
 	main_Layout->addWidget(main_Table);
+	resize(800,300);
 }
 
 void Table_Of_Structures::create_Table()
 {
+	// PARAMETER
+
 	read_Trees();
 	main_Table = new QTableWidget(basic_Row_Number, basic_Column_Number, this);
+	rows_List_To_Span.clear();
 
-	// set prototype
-	QTableWidgetItem* proto_Item = new QTableWidgetItem("prototype");
-	proto_Item->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEnabled);
-	main_Table->setItemPrototype(proto_Item);
-
-	for(int i=0; i<multilayer_Tabs->count(); ++i)
+	for(int tab_Index=0; tab_Index<multilayer_Tabs->count(); ++tab_Index)
 	{
 		// header for each structure
 		main_Table->insertRow(main_Table->rowCount());
-						int current_Row = main_Table->rowCount()-1;
-		main_Table->setSpan(current_Row,0,1,basic_Column_Number);
-		main_Table->setItem(current_Row,0, new QTableWidgetItem(multilayer_Tabs->tabText(i)));
+		int current_Row = main_Table->rowCount()-1;
+		rows_List_To_Span.append(current_Row);
+		main_Table->setItem(current_Row,0, new QTableWidgetItem(multilayer_Tabs->tabText(tab_Index)));
 		main_Table->item   (current_Row,0)->setTextAlignment(Qt::AlignCenter);
+		main_Table->item   (current_Row,0)->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEnabled);
 
 		main_Table->insertRow(main_Table->rowCount());
 
 		// structure display
+		int max_Depth = Global_Variables::get_Tree_Depth(list_Of_Trees[tab_Index]->tree->invisibleRootItem());
 		int depth;
 		QTreeWidgetItem* structure_Item;
-		QTreeWidgetItemIterator it(list_Of_Trees[i]->tree);
+		QTreeWidgetItemIterator it(list_Of_Trees[tab_Index]->tree);
 		while (*it)
 		{
 			structure_Item = *it;
 			depth = Global_Variables::get_Item_Depth(structure_Item);
-
 			main_Table->insertRow(main_Table->rowCount());
-								current_Row = main_Table->rowCount()-1;
+			current_Row = main_Table->rowCount()-1;
+
+			// print whatsThis
+			add_Columns(depth);
 			main_Table->setItem(current_Row,depth, new QTableWidgetItem(structure_Item->whatsThis(DEFAULT_COLUMN)));
+			{
+				main_Table->item(current_Row,depth)->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEnabled);
+			}
+
+			// load data
+			QStringList whats_This_List = structure_Item->whatsThis(DEFAULT_COLUMN).split(item_Type_Delimiter,QString::SkipEmptyParts);
+			if(whats_This_List[0] == whats_This_Ambient)	{item_Type = Item_Type::Ambient;		ambient		  = structure_Item->data(DEFAULT_COLUMN, Qt::UserRole).value<Ambient>();		}
+			if(whats_This_List[0] == whats_This_Layer)		{item_Type = Item_Type::Layer;			layer		  = structure_Item->data(DEFAULT_COLUMN, Qt::UserRole).value<Layer>();			}
+			if(whats_This_List[0] == whats_This_Multilayer) {item_Type = Item_Type::Stack_Content;	stack_Content = structure_Item->data(DEFAULT_COLUMN, Qt::UserRole).value<Stack_Content>();	}
+			if(whats_This_List[0] == whats_This_Substrate)	{item_Type = Item_Type::Substrate;		substrate	  = structure_Item->data(DEFAULT_COLUMN, Qt::UserRole).value<Substrate>();		}
+
+			// common variables
+			int current_Column = -5;
+			bool composed_Material;
+			QString material = "empty";
+			QList<Stoichiometry> composition;
+
+			if(item_Type == Item_Type::Ambient)
+			{
+				composed_Material = ambient.composed_Material;
+				composition = ambient.composition;
+				material = ambient.material;
+			}
+			if(item_Type == Item_Type::Layer)
+			{
+				composed_Material = layer.composed_Material;
+				composition = layer.composition;
+				material = layer.material;
+			}
+			if(item_Type == Item_Type::Stack_Content)
+			{
+
+			}
+			if(item_Type == Item_Type::Substrate)
+			{
+				composed_Material = substrate.composed_Material;
+				composition = substrate.composition;
+				material = substrate.material;
+			}
+
+			current_Column = max_Depth+2;
+
+			if(item_Type == Item_Type::Ambient || item_Type == Item_Type::Layer || item_Type == Item_Type::Substrate)
+			if(composed_Material)
+			{
+				for(int comp_Index=0; comp_Index<composition.size(); ++comp_Index)
+				{
+					add_Columns(current_Column);
+					main_Table->setItem(current_Row,current_Column, new QTableWidgetItem(composition[comp_Index].type));
+					main_Table->item(current_Row,current_Column)->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEnabled);
+					++current_Column;
+				}
+			} else
+			{
+				add_Columns(current_Column);
+				main_Table->setItem(current_Row,current_Column, new QTableWidgetItem(material));
+				main_Table->item(current_Row,current_Column)->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEnabled);
+				++current_Column;
+			}
+
+
+
+
 			++it;
 		}
 
 		main_Table->insertRow(main_Table->rowCount());
 	}
+	span_Title_Rows();
 	main_Table->resizeColumnsToContents();
+	main_Table->resizeRowsToContents();
 }
 
 void Table_Of_Structures::read_Trees()
@@ -68,6 +136,22 @@ void Table_Of_Structures::read_Trees()
 		Structure_Tree* old_Structure_Tree = dynamic_cast<Multilayer*>(multilayer_Tabs->widget(i))->structure_Tree;
 		list_Of_Trees.append(old_Structure_Tree);
 		old_Structure_Tree->structure_Toolbar->toolbar->setDisabled(true);
+	}
+}
+
+void Table_Of_Structures::add_Columns(int add_After)
+{
+	while(add_After>=main_Table->columnCount())
+	{
+		main_Table->insertColumn(main_Table->columnCount());
+	}
+}
+
+void Table_Of_Structures::span_Title_Rows()
+{
+	for(int i=0; i<rows_List_To_Span.size(); ++i)
+	{
+		main_Table->setSpan(rows_List_To_Span[i],0,1,main_Table->columnCount());
 	}
 }
 
--- source/multilayer_approach/table_of_structures.h
+++ source/multilayer_approach/table_of_structures.h
@@ -22,15 +22,27 @@ public:
 		void create_Table();
 		void read_Trees();
 
+	void add_Columns(int add_After);
+	void span_Title_Rows();
+
 	int basic_Row_Number = 0;
-	int basic_Column_Number = 20;
+	int basic_Column_Number = 1;
 
 	QTableWidget* main_Table;
 	QList<Structure_Tree*> list_Of_Trees;
 private:
+	QList<int> rows_List_To_Span;
 	QHBoxLayout* main_Layout;
 	QMap<QString, Table_Of_Structures*>* runned_Tables_Of_Structures;
 	QTabWidget* multilayer_Tabs;
+
+	Item_Type item_Type;
+
+//	Measurement measurement;
+	Ambient ambient;
+	Layer layer;
+	Stack_Content stack_Content;
+	Substrate substrate;
 };
 
 #endif // TABLE_OF_STRUCTURES_H
