0a456c5ea5098ad4b0bd028b2302d58d4ff97dd9
Michael Svechnikov <svechnikovmv@gmail.com>
2021-10-05 19:50:42 +0300

    Profile plot X unscaled

--- source/global/global_definitions.h
+++ source/global/global_definitions.h
@@ -741,12 +741,12 @@ struct Profile_Plot_Options		{QString type = PERMITTIVITY;
 								 bool show_Discretization = false;
 								 bool show_Cursor_Position = false;
 
-								 bool rescale_X = true;
+								 bool rescale_X = false;
 								 bool rescale_Y = true;
 								 QString y_Scale = lin_Scale;
 
 								 double old_X_Begin = -10;
-								 double old_X_End   = +200;
+								 double old_X_End   = +500;
 								 double old_Y_Begin = 0;
 								 double old_Y_End   = 1;
 
