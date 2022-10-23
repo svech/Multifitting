55cc8a783934ca386d2e9b0102e94286b9efd006
Mikhail Svechnikov <svechnikovmv@gmail.com>
2022-10-23 12:19:22 +0200

    Profile plot: rm depth units menu

--- source/multilayer_approach/profile_plots/profile_plots_window.cpp
+++ source/multilayer_approach/profile_plots/profile_plots_window.cpp
@@ -39,41 +39,8 @@ void Profile_Plots_Window::contextMenuEvent(QContextMenuEvent *event)
 		}
 	}
 
-    QMenu depth_Units("Depth units");
-    {
-        QActionGroup* group_Act_Unit = new QActionGroup(this);
-            group_Act_Unit->setExclusive(true);
-
-        for(int index=0; index<length_Units_List.size(); index++)
-        {
-            QAction* act_Unit = new QAction(length_Units_List[index], this);
-                act_Unit->setProperty(index_Property, index);
-                act_Unit->setCheckable(true);
-                act_Unit->setActionGroup(group_Act_Unit);
-
-            if(length_Units_List[index] == multilayer->profile_Plot_Options.local_length_units) {act_Unit->setChecked(true);}
-            depth_Units.addAction(act_Unit);
-            connect(act_Unit,  &QAction::triggered, this, [=]
-            {
-                double old_Factor = length_Coefficients_Map.value(multilayer->profile_Plot_Options.local_length_units);
-                multilayer->profile_Plot_Options.local_length_units = length_Units_List[index];
-                double new_Factor = length_Coefficients_Map.value(multilayer->profile_Plot_Options.local_length_units);
-
-                profile_Plot_Vector[main_Tabs->currentIndex()]->custom_Plot->xAxis->setLabel("Depth, "+multilayer->profile_Plot_Options.local_length_units);
-
-                multilayer->profile_Plot_Options.old_X_Begin = multilayer->profile_Plot_Options.old_X_Begin / new_Factor * old_Factor;
-                multilayer->profile_Plot_Options.old_X_End   = multilayer->profile_Plot_Options.old_X_End   / new_Factor * old_Factor;
-
-                profile_Plot_Vector[main_Tabs->currentIndex()]->horizontall_Scrollbar->blockSignals(true);
-                profile_Plot_Vector[main_Tabs->currentIndex()]->plot_Data(true);
-                profile_Plot_Vector[main_Tabs->currentIndex()]->horizontall_Scrollbar->blockSignals(false);
-            });
-        }
-    }
-
 	QMenu menu;
 	menu.addMenu(&wavelength_Units);
-    menu.addMenu(&depth_Units);
     menu.exec(event->globalPos());
 }
 
